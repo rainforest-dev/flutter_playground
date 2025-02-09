214212d43db143f31bf05163ad1b67f04b32dd7c
Rainforest Cheng <contact@rainforest.tools>
2025-02-09 15:53:58 +0800

    feat: Integrate mobile_scanner for QR code scanning functionality and update project configurations

--- lib/screens/scan.dart
+++ lib/screens/scan.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
@@ -8,21 +9,44 @@ class ScanScreen extends StatefulWidget {
 }
 
 class _ScanScreenState extends State<ScanScreen> {
+  Barcode? _barcode;
+
+  Widget _buildBarcode(Barcode? value) {
+    if (value == null) {
+      return const Text('Scan something!', overflow: TextOverflow.fade);
+    }
+    return Text(value.displayValue ?? 'Unknown barcode',
+        overflow: TextOverflow.fade);
+  }
+
+  void _handleBarcode(BarcodeCapture capture) {
+    if (mounted) {
+      setState(() {
+        _barcode = capture.barcodes.firstOrNull;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Scan Screen'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: Center(
-          child: Column(
-        children: [
-          const Text('This is the Scan Screen'),
-          ElevatedButton(
-              onPressed: () => context.pop(), child: const Text('Start Scan'))
-        ],
-      )),
-    );
+        appBar: AppBar(
+          title: const Text('Scan Screen'),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: Stack(children: [
+          MobileScanner(
+            onDetect: _handleBarcode,
+          ),
+          Center(
+              child: Column(
+            children: [
+              _buildBarcode(_barcode),
+              ElevatedButton(
+                  onPressed: () => context.pop(),
+                  child: const Text('Start Scan'))
+            ],
+          )),
+        ]));
   }
 }
